2ed5603f4a6a196787f856482a79503423397952
gsf <wb-gsf605812@alibaba-inc.com>
2021-11-03 11:04:22 +0800

    feat:init

--- generators/app/index.js
+++ generators/app/index.js
@@ -8,9 +8,33 @@ const Generator = require('yeoman-generator');
 module.exports = class extends (
   Generator
 ) {
+  prompting() {
+    //yeoman在询问用户环节会自动调用此方法
+    //在此方法中可以调用父类的prompt()方法发出对用户的命令行询问
+    return this.prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'Your project name',
+        default: this.appname, //appname 为项目生成目录名称
+      },
+    ]).then((answers) => {
+      //answers => { name: 'user input value'}
+      this.answers = answers;
+    });
+  }
   writing() {
     // yeoman 自动在生成文件阶段调用此方法
     //我们这里尝试往项目目录中写入文件
-    this.fs.write(this.destinationPath('temp.txt'), Math.random().toString());
+    //   this.fs.write(this.destinationPath('temp.txt'), Math.random().toString());
+    //通过模板方式写入文件到文件目录
+    //模板文件路径
+    const tmpl = this.templatePath('bar.html');
+    //输出目标路径
+    const output = this.destinationPath('bar.html');
+    //模板数据上下文
+    const context = this.answers;
+
+    this.fs.copyTpl(tmpl, output, context);
   }
 };
